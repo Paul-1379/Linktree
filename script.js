2a3a0bb517645e6993f3cf805d8cea70c6772f06
Paul1379 <raphael.vasse@outlook.com>
2024-11-10 19:48:24 +0100

    Improve main page
    - add icon and title
    - remove robloc link

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 window.onload = function(){
     const container = document.getElementById('data-container');
-    var itemDiv = document.createElement('div');
-
-    // Ajouter le contenu HTML avec les données de l'item
-    itemDiv.innerHTML = `
-        <li><h3><a href="https://paul-1379.github.io/Linktree/Data.json">https://paul-1379.github.io/Linktree/Data.json</a></h3></li>
-    `;
+    
 
     fetch('./Data.json')
         .then(res => res.json())
         .then(data => {
-            console.log(data);
             data.forEach(element => {
-                itemDiv.innerHTML = `<li>${element.title}</li>`;
-                console.log(`${element.title}`);
+                var itemDiv = document.createElement('div');
+                itemDiv.innerHTML = `<a href = ${element.link}>${element.title}</a>`;
                 container.appendChild(itemDiv);
             });
         })
