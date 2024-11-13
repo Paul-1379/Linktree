b5b6f335b65b2869aca95f2398d16dd084758fcf
Paul1379 <raphael.vasse@outlook.com>
2024-11-13 15:13:27 +0100

    Add background and improve links

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 window.onload = function(){
-    const container = document.getElementById('data-container');
+    const container = document.getElementById('links');
     
 
     fetch('./Data.json')
         .then(res => res.json())
         .then(data => {
             data.forEach(element => {
-                var itemDiv = document.createElement('div');
-                itemDiv.innerHTML = `<a href = ${element.link}>${element.title}</a>`;
-                container.appendChild(itemDiv);
+                var link = document.createElement('a');
+                link.setAttribute('href', `${element.link}`);
+                link.innerHTML = `<img src="${element.logo}" alt=${element.title} class="logo link">`;
+                container.appendChild(link);
             });
         })
 }
