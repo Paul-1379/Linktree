2d18e7d7459fc80e781ee3f7832cb6cc74d66a5f
Paul1379 <raphael.vasse@outlook.com>
2024-11-16 12:17:33 +0100

    Finish linktree

--- script.js
+++ script.js
@@ -8,8 +8,23 @@ window.onload = function(){
             data.forEach(element => {
                 var link = document.createElement('a');
                 link.setAttribute('href', `${element.link}`);
-                link.innerHTML = `<img src="${element.logo}" alt=${element.title} class="logo link">`;
+                link.setAttribute('style', `color: #fe2f00`);
+                link.setAttribute('class', `link-item`)
+
+                var logoImg = document.createElement('img');
+                logoImg.setAttribute('src', `${element.logo}`);
+                logoImg.setAttribute("alt", `${element.title}`);
+                logoImg.setAttribute("class", "logo link")
+                link.appendChild(logoImg);
+                
+                var title = document.createElement('div');
+                title.innerHTML = `<h2>${element.title}</h2>`;
+                link.appendChild(title);
+                var description = document.createElement('div');
+                description.innerHTML = `${element.description}`;
+                
                 container.appendChild(link);
+                link.appendChild(description);
             });
         })
 }
